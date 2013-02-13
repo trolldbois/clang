dd933c76c2afc0cce580c93ddfc5f04871a5e64c
Loic Jaquemet <loic.jaquemet+python@gmail.com>
2013-02-12 22:46:25 -0500

    fix tests cases

--- test/Index/vector-types.c
+++ test/Index/vector-types.c
@@ -1,6 +1,6 @@
 int __attribute__((vector_size(16))) x;
 typedef int __attribute__((vector_size(16))) int4_t;
 
-// RUN: c-index-test -test-print-typekind %s | FileCheck %s
+// RUN: c-index-test -test-print-typekind %s -target i386-linux-gnu | FileCheck %s
 // CHECK: VarDecl=x:1:38 typekind=Vector [isPOD=1]
-// CHECK: TypedefDecl=int4_t:2:46 (Definition) typekind=Typedef [canonical=Vector] [isPOD=1]
+// CHECK: TypedefDecl=int4_t:2:46 (Definition) typekind=Typedef [canonical=Vector] [size=128] [alignment=128] [isPOD=1]
